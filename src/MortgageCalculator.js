28059caed09212eab5639a528b20b5f493c44585
Marv <mrvncaragay@gmail.com>
2021-07-27 21:03:10 -0700

    Set Mortgage Calculator default colors

--- src/MortgageCalculator.js
+++ src/MortgageCalculator.js
@@ -1,12 +1,15 @@
 import {StatusBar} from 'expo-status-bar'
 import React from 'react'
 import {StyleSheet, Text, View} from 'react-native'
-import fontFamily from '../src/styles/fontFamily'
+import fontFamily from './styles/fontFamily'
+import colors from './styles/colors'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{fontFamily: fontFamily.MONTSERRAT_SEMIBOLD, fontSize: 20}}>Calculator Mortgage</Text>
+      <Text style={{fontFamily: fontFamily.MONTSERRAT_BOLD, fontSize: 20, color: colors.green}}>
+        Calculator Mortgage
+      </Text>
       <StatusBar style="auto" />
     </View>
   )
---
+++ src/styles/colors.js
@@ -0,0 +1,14 @@
+export default {
+  white: '#FFFFFF',
+  black: '#000000',
+  gray200: '#E2E8F0',
+  gray300: '#CBD5E1',
+  gray400: '#94A3B8',
+  gray500: '#64748B',
+  gray600: '#475569',
+  teal: '#2DD4BF',
+  cyan: '#22D3EE',
+  green: '#4ADE80',
+  violet: '#A78BFA',
+  transparent: 'transparent',
+}
