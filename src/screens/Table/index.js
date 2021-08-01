8f6cb0355bdea94ac510e045881d352f21d563ca
Marv <mrvncaragay@gmail.com>
2021-08-01 14:08:34 -0700

    Reduce table header and Item size when screen width is smaller

--- src/screens/Table/index.js
+++ src/screens/Table/index.js
@@ -1,21 +1,28 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, useWindowDimensions} from 'react-native'
 import styles from '../../styles/styles'
 import {Container, Picker, FlatList} from 'components'
 import {useBasicMortgageCalculator} from 'context/basicMortgageCalculator'
 import {formatNumber} from 'utils/formatter'
 import calculateMonthlyYearlyPayment from '../../utils/calculateMonthlyYearlyPayment'
 
+const headerFontSize = width => {
+  if (width >= 380) return {}
+  return size => ({fontSize: size})
+}
+
 function ListHeader({term}) {
+  const screenWidth = useWindowDimensions().width
+  const headerFontSizeStyle = headerFontSize(screenWidth)(11)
   return (
     <View style={[styles.listTableHeaderContainer]}>
-      <Text style={[styles.listTableHeader, term === 'year' ? {width: 50} : {width: 75}]}>
+      <Text style={[styles.listTableHeader, term === 'year' ? {width: 50} : {width: 75}, headerFontSize(screenWidth)]}>
         {term === 'year' ? 'YEAR' : 'MONTH'}
       </Text>
-      <Text style={[styles.listTableHeader, {flex: 1}]}>INTEREST</Text>
-      <Text style={[styles.listTableHeader, {flex: 1}]}>PRINCIPAL</Text>
-      <Text style={[styles.listTableHeader, {flex: 1}]}>TOTAL</Text>
-      <Text style={[styles.listTableHeader, {flex: 1}]}>BALANCE</Text>
+      <Text style={[styles.listTableHeader, {flex: 1}, headerFontSizeStyle]}>INTEREST</Text>
+      <Text style={[styles.listTableHeader, {flex: 1}, headerFontSizeStyle]}>PRINCIPAL</Text>
+      <Text style={[styles.listTableHeader, {flex: 1}, headerFontSizeStyle]}>TOTAL</Text>
+      <Text style={[styles.listTableHeader, {flex: 1}, headerFontSizeStyle]}>BALANCE</Text>
     </View>
   )
 }
@@ -23,13 +30,15 @@ ListHeader = React.memo(ListHeader)
 
 function ListItem({term, data}) {
   const {label, interest, principal, total, balance} = data
+  const screenWidth = useWindowDimensions().width
+  const headerFontSizeStyle = headerFontSize(screenWidth)(12)
   return (
     <View style={styles.listItemTableContainer}>
       <Text style={[styles.listItemTable, term === 'year' ? {width: 50} : {width: 75}]}>{label}</Text>
-      <Text style={[styles.listItemTable, {flex: 1}]}>${formatNumber(interest)}</Text>
-      <Text style={[styles.listItemTable, {flex: 1}]}>${formatNumber(principal)}</Text>
-      <Text style={[styles.listItemTable, {flex: 1}]}>${formatNumber(total)}</Text>
-      <Text style={[styles.listItemTable, {flex: 1}]}>${formatNumber(balance)}</Text>
+      <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(interest)}</Text>
+      <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(principal)}</Text>
+      <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(total)}</Text>
+      <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(balance)}</Text>
     </View>
   )
 }
@@ -39,7 +48,6 @@ ListItem = React.memo(ListItem)
 function Table() {
   const [{basic}] = useBasicMortgageCalculator()
   const {tableData} = calculateMonthlyYearlyPayment(basic)
-
   const [term, setTerm] = React.useState('year')
 
   return (
