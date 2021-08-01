516215f6a1f82f6800c38d042d4c2ccfd1f5728f
Marv <mrvncaragay@gmail.com>
2021-08-01 14:32:07 -0700

    Refractor headerFontSize function

--- src/screens/Table/index.js
+++ src/screens/Table/index.js
@@ -6,17 +6,18 @@ import {useBasicMortgageCalculator} from 'context/basicMortgageCalculator'
 import {formatNumber} from 'utils/formatter'
 import calculateMonthlyYearlyPayment from '../../utils/calculateMonthlyYearlyPayment'
 
-const headerFontSize = width => {
+const headerFontSize = (width, size) => {
   if (width >= 380) return {}
-  return size => ({fontSize: size})
+  return {fontSize: size}
 }
 
 function ListHeader({term}) {
   const screenWidth = useWindowDimensions().width
-  const headerFontSizeStyle = headerFontSize(screenWidth)(11)
+  const headerFontSizeStyle = headerFontSize(screenWidth, 11)
+
   return (
     <View style={[styles.listTableHeaderContainer]}>
-      <Text style={[styles.listTableHeader, term === 'year' ? {width: 50} : {width: 75}, headerFontSize(screenWidth)]}>
+      <Text style={[styles.listTableHeader, term === 'year' ? {width: 50} : {width: 75}, headerFontSizeStyle]}>
         {term === 'year' ? 'YEAR' : 'MONTH'}
       </Text>
       <Text style={[styles.listTableHeader, {flex: 1}, headerFontSizeStyle]}>INTEREST</Text>
@@ -31,10 +32,12 @@ ListHeader = React.memo(ListHeader)
 function ListItem({term, data}) {
   const {label, interest, principal, total, balance} = data
   const screenWidth = useWindowDimensions().width
-  const headerFontSizeStyle = headerFontSize(screenWidth)(12)
+  const headerFontSizeStyle = headerFontSize(screenWidth, 12)
   return (
     <View style={styles.listItemTableContainer}>
-      <Text style={[styles.listItemTable, term === 'year' ? {width: 50} : {width: 75}]}>{label}</Text>
+      <Text style={[styles.listItemTable, term === 'year' ? {width: 50} : {width: 75}, headerFontSizeStyle]}>
+        {label}
+      </Text>
       <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(interest)}</Text>
       <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(principal)}</Text>
       <Text style={[styles.listItemTable, {flex: 1}, headerFontSizeStyle]}>${formatNumber(total)}</Text>
