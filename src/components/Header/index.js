2ada4c7d795298e2f6c0048838b2ca06afdd32b8
Marv <mrvncaragay@gmail.com>
2021-08-02 11:12:11 -0700

    Refractor Header to be more dynamic

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,16 +5,11 @@ import styles from '../../styles/styles'
 import variables from '../../styles/variables'
 import {Ionicons} from '@expo/vector-icons'
 
-const Header = ({style, iconColor, textStyle, iconName, navigation}) => {
+const Header = ({label = 'Mortgage Calculator', style, iconColor, textStyle, iconName, onPress}) => {
   return (
     <View style={[styles.headerContainer, style]}>
-      <Ionicons
-        name={iconName}
-        size={variables.iconSizeMedium}
-        color={colors[iconColor]}
-        onPress={() => navigation.toggleDrawer()}
-      />
-      <Text style={[styles.headerTitle, textStyle]}>Mortgage Calculator</Text>
+      <Ionicons name={iconName} size={variables.iconSizeMedium} color={colors[iconColor]} onPress={onPress} />
+      <Text style={[styles.headerTitle, textStyle]}>{label}</Text>
     </View>
   )
 }
