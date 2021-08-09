2a157ec87a9fa4efa2aebc04ec45a9ec705c87f2
Marv <mrvncaragay@gmail.com>
2021-08-08 23:44:45 -0700

    Refactor Detail Advance

--- src/screens/Detail/components/Advance.js
+++ src/screens/Detail/components/Advance.js
@@ -5,164 +5,44 @@ import {MaterialIcons} from '@expo/vector-icons'
 import variables from 'styles/variables'
 import {Container, TextInput} from 'components'
 import {useAdvanceMortgageCalculator} from 'context/advanceMortgageCalculator'
-import {formatDate, formatNumber} from 'utils/formatter'
 import calculateDetailAdvance from '../../../utils/calculateDetailAdvance'
-import moment from 'moment'
 import numbro from 'numbro'
 
 function Detail() {
   const [{advance}] = useAdvanceMortgageCalculator()
-  const {
-    homeValue,
-    mortgageAmount,
-    paymentFrequency,
-    monthlyOrBiWeeklyExtraPayment,
-    propertyTax,
-    homeInsurance,
-    pmi,
-    hoaFees,
-    totalPayment,
-    numberOfPayments,
-    startDate,
-    endDate,
-    downPayment,
-    totalExtraPayment,
-    principal,
-    totalFees,
-    totalAllPayments,
-    totalInterest,
-  } = calculateDetailAdvance(advance)
+  const {data} = calculateDetailAdvance(advance)
 
   return (
     <Container>
       <ScrollView>
-        <TextInput
-          label="Home Value"
-          editable={false}
-          value={formatNumber(homeValue)}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Mortgage Amount"
-          editable={false}
-          value={formatNumber(mortgageAmount)}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label={`${paymentFrequency.type} Principal & Interest`}
-          editable={false}
-          value={numbro(paymentFrequency.amount).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label={`${paymentFrequency.type} Extra Payment`}
-          editable={false}
-          value={numbro(monthlyOrBiWeeklyExtraPayment).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label={`${paymentFrequency.type} Property Tax`}
-          editable={false}
-          value={numbro(propertyTax).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label={`${paymentFrequency.type} Home Insurance`}
-          editable={false}
-          value={numbro(homeInsurance).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label={`${paymentFrequency.type} PMI (Until ${moment(new Date()).add(44, 'months').format('MMM, YYYY')})`}
-          editable={false}
-          value={numbro(pmi).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label={`${paymentFrequency.type} HOA Fees`}
-          editable={false}
-          value={numbro(hoaFees).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Total Monthly Payment"
-          editable={false}
-          value={numbro(totalPayment).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Number Of Payments"
-          editable={false}
-          value={numbro(numberOfPayments).format({thousandSeparated: true})}
-        />
-
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <TextInput
-            containerStyle={{width: '45%'}}
-            inputStyle={{fontSize: 14}}
-            label="Start Date"
-            editable={false}
-            value={formatDate(startDate, 'MMM YYYY')}
-          />
-          <TextInput
-            containerStyle={{width: '45%'}}
-            inputStyle={{fontSize: 14}}
-            label="Pay Off Date"
-            editable={false}
-            value={formatDate(endDate, 'MMM YYYY')}
-          />
-        </View>
-
-        <TextInput
-          label="Down Payment"
-          editable={false}
-          value={numbro(downPayment).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Principal"
-          editable={false}
-          value={numbro(principal).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Total Extra Payment"
-          editable={false}
-          value={numbro(totalExtraPayment).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Total Interest Paid"
-          editable={false}
-          value={numbro(totalInterest).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Total Tax, Insurance, PMI & Fees"
-          editable={false}
-          value={numbro(totalFees).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
-
-        <TextInput
-          label="Total Of All Payments"
-          editable={false}
-          value={numbro(totalAllPayments).format({thousandSeparated: true, mantissa: 2})}
-          icon={<MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />}
-        />
+        {data.map(({label, value, mantissa = 2, icon = true, dates, date, id}) => {
+          return date ? (
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}} key={id}>
+              {dates.map(({label, value}) => (
+                <TextInput
+                  key={label}
+                  containerStyle={{width: '45%'}}
+                  inputStyle={{fontSize: 14}}
+                  label={label}
+                  editable={false}
+                  value={value}
+                />
+              ))}
+            </View>
+          ) : (
+            <TextInput
+              key={id}
+              label={label}
+              editable={false}
+              value={numbro(value).format({thousandSeparated: true, mantissa})}
+              icon={
+                icon ? (
+                  <MaterialIcons name="attach-money" size={variables.iconSizeMedium} color={colors.gray400} />
+                ) : null
+              }
+            />
+          )
+        })}
       </ScrollView>
     </Container>
   )
--- src/utils/calculateDetailAdvance.js
+++ src/utils/calculateDetailAdvance.js
@@ -1,5 +1,5 @@
 import calculateExtraPaymentsAndInterest from './calculateExtraPaymentAndInterest'
-import {unformat} from '../utils/formatter'
+import {unformat, formatDate} from '../utils/formatter'
 import moment from 'moment'
 
 export default data => {
@@ -68,21 +68,107 @@ export default data => {
   const totalAllPayments = totalFees + totalInterest + mortgageAmount + downPayment
 
   return {
-    ...data,
-    paymentFrequency,
-    monthlyOrBiWeeklyExtraPayment: monthlyOrBiWeeklyPayment,
-    propertyTax,
-    homeInsurance,
-    pmi,
-    hoaFees,
-    totalPayment: totalPaymentMonthly,
-    numberOfPayments: months, //fixed pay off date displayed use moment to get the date
-    endDate,
-    downPayment,
-    principal,
-    totalExtraPayment: totalExtraPayment,
-    totalInterest: totalInterest,
-    totalFees,
-    totalAllPayments,
+    data: [
+      {
+        label: 'Home Value',
+        value: hv,
+        id: 11,
+      },
+      {
+        label: 'Mortgage Amount',
+        value: mortgageAmount,
+        id: 12,
+      },
+      {
+        label: `${paymentFrequency.type} Principal & Interest`,
+        value: paymentFrequency.amount,
+        id: 13,
+      },
+      {
+        label: `${paymentFrequency.type} Extra Payment`,
+        value: monthlyOrBiWeeklyPayment,
+        id: 14,
+      },
+      {
+        label: `${paymentFrequency.type} Property Tax`,
+        value: propertyTax,
+        id: 15,
+      },
+      {
+        label: `${paymentFrequency.type} Home Insurance`,
+        value: homeInsurance,
+        id: 16,
+      },
+      {
+        label: `${paymentFrequency.type} PMI (Until ${moment(new Date())
+          .add(pmiDuration - 1, 'months')
+          .format('MMM, YYYY')})`,
+        value: pmi,
+        id: 17,
+      },
+      {
+        label: `${paymentFrequency.type} HOA Fees`,
+        value: hoaFees,
+        id: 18,
+      },
+      {
+        label: 'Total Monthly Payment',
+        value: totalPaymentMonthly,
+        id: 19,
+      },
+      {
+        label: 'Number Of Payments',
+        value: months,
+        mantissa: 0,
+        icon: null,
+        id: 20,
+      },
+      {
+        dates: [
+          {
+            label: 'Start Date',
+            value: formatDate(startDate, 'MMM YYYY'),
+            id: 21,
+          },
+          {
+            label: 'Pay Off Date',
+            value: formatDate(endDate, 'MMM YYYY'),
+            id: 22,
+          },
+        ],
+        date: true,
+        id: 30,
+      },
+      {
+        label: 'Down Payment',
+        value: downPayment,
+        id: 23,
+      },
+      {
+        label: 'Principal',
+        value: principal,
+        id: 24,
+      },
+      {
+        label: 'Total Extra Payment',
+        value: totalExtraPayment,
+        id: 25,
+      },
+      {
+        label: 'Total Interest Paid',
+        value: totalInterest,
+        id: 26,
+      },
+      {
+        label: 'Total Tax, Insurance, PMI & Fees',
+        value: totalFees,
+        id: 27,
+      },
+      {
+        label: 'Total Of All Payments',
+        value: totalAllPayments,
+        id: 28,
+      },
+    ],
   }
 }
