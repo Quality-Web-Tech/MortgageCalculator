48dc1815005e156fef8dfc05019880cec15477b1
Marv <mrvncaragay@gmail.com>
2021-08-08 22:30:23 -0700

    Fix replaceAll nundefined on android

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -3,21 +3,21 @@ import moment from 'moment'
 const formatDate = (date, defaultFormat = 'MMMM DD, YYYY') => moment(date).format(defaultFormat)
 
 const formatNumber = num => {
-  if (num === undefined) return ''
   if (num === '') return ''
 
-  num = num.toString()
-  const arrStringNum = num.split('.')
+  const stringNum = String(num)
+  const arrStringNum = stringNum.split('.')
 
   if (arrStringNum.length > 2) return arrStringNum[0] + '.' + arrStringNum[1]
-  if (arrStringNum.length === 1) return num.replaceAll(',', '').replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  if (arrStringNum.length === 1) return stringNum.replace(/,/g, '', '').replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+
   return arrStringNum[0] + '.' + arrStringNum[1]
 }
 
 const unformat = num => {
   return {
     orig: num,
-    formatted: Number(formatNumber(num).replaceAll(',', '')),
+    formatted: Number(formatNumber(num).replace(/,/g, '')),
   }
 }
 
